c9434c36fefb15122b469065cb416865f1efa9b6
Jack Graves [AC] <jack.graves@automation-consultants.com>
2021-01-05 22:35:01 +0000

    Add types

--- dist/index.js
+++ dist/index.js
@@ -8,39 +8,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.OptroPowerupApi = exports.OptroLicenseApi = void 0;
-var cache_1 = require("./cache");
-var api_1 = require("./api");
-var OptroLicenseApi = (function () {
-    function OptroLicenseApi(apiKey, powerupId, maxAge, interval) {
+const cache_1 = require("./cache");
+const api_1 = require("./api");
+class OptroLicenseApi {
+    constructor(apiKey, powerupId, maxAge, interval) {
         this.cache = null;
         this.apiKey = null;
         this.powerUpId = null;
@@ -48,51 +21,39 @@ var OptroLicenseApi = (function () {
         this.powerUpId = powerupId;
         this.cache = new cache_1.default(maxAge, interval);
     }
-    OptroLicenseApi.prototype.getBoardLicenseStatus = function (boardId) {
-        return __awaiter(this, void 0, void 0, function () {
-            var cachedValue, liveValue;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        cachedValue = this.cache.get("board_" + boardId);
-                        if (!(typeof cachedValue !== "undefined")) return [3, 1];
-                        return [2, cachedValue];
-                    case 1: return [4, api_1.fetchLicenseStatus({ powerupId: this.powerUpId, boardId: boardId }, this.apiKey)];
-                    case 2:
-                        liveValue = _a.sent();
-                        this.cache.set("board_" + boardId, liveValue, undefined);
-                        return [2, liveValue];
-                }
-            });
+    getBoardLicenseStatus(boardId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const cachedValue = this.cache.get(`board_${boardId}`);
+            if (typeof cachedValue !== "undefined") {
+                return cachedValue;
+            }
+            else {
+                const liveValue = yield api_1.fetchLicenseStatus({ powerupId: this.powerUpId, boardId: boardId }, this.apiKey);
+                this.cache.set(`board_${boardId}`, liveValue, undefined);
+                return liveValue;
+            }
         });
-    };
-    OptroLicenseApi.prototype.getMemberLicenseStatus = function (memberId) {
-        return __awaiter(this, void 0, void 0, function () {
-            var cachedValue, liveValue;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        cachedValue = this.cache.get("member_" + memberId);
-                        if (!(typeof cachedValue !== "undefined")) return [3, 1];
-                        return [2, cachedValue];
-                    case 1: return [4, api_1.fetchLicenseStatus({ powerupId: this.powerUpId, memberId: memberId }, this.apiKey)];
-                    case 2:
-                        liveValue = _a.sent();
-                        this.cache.set("member_" + memberId, liveValue, undefined);
-                        return [2, liveValue];
-                }
-            });
+    }
+    getMemberLicenseStatus(memberId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const cachedValue = this.cache.get(`member_${memberId}`);
+            if (typeof cachedValue !== "undefined") {
+                return cachedValue;
+            }
+            else {
+                const liveValue = yield api_1.fetchLicenseStatus({ powerupId: this.powerUpId, memberId: memberId }, this.apiKey);
+                this.cache.set(`member_${memberId}`, liveValue, undefined);
+                return liveValue;
+            }
         });
-    };
-    return OptroLicenseApi;
-}());
+    }
+}
 exports.OptroLicenseApi = OptroLicenseApi;
-var OptroPowerupApi = (function () {
-    function OptroPowerupApi(apiKey) {
+class OptroPowerupApi {
+    constructor(apiKey) {
         this.apiKey = null;
         this.apiKey = apiKey;
     }
-    return OptroPowerupApi;
-}());
+}
 exports.OptroPowerupApi = OptroPowerupApi;
 //# sourceMappingURL=index.js.map
