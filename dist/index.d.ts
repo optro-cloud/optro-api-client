d65a636c7ab5ba19fad07b7cbd3b173e8d14896c
Jeff Tomband <Jeff.tomband@outlook.com>
2021-06-09 09:19:16 +0100

    Added types export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,5 @@
+import * as Types from "./types/types";
 export { default as OptroLicenseApi } from './OproLicenseApi';
 export { default as Cache } from './cache';
 export { fetchLicenseStatus as fetchLicenseStatus } from './api';
+export { Types };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import * as Types from "./types/types";
+
 export { default as OptroLicenseApi } from './OproLicenseApi';
 export { default as Cache } from './cache';
 export { fetchLicenseStatus as fetchLicenseStatus } from './api';
+
+export { Types }
